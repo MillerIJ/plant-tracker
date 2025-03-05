f942e6e918d4cf1555eea5060e7c3ca0c17a7122
MillerIJ <millerisabel38@gmail.com>
2025-03-05 17:38:33 -0600

    created current date functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useData } from "../context/DataContext";
+
+/* SAMPLE DATA
+plants = [
+  {
+    id: 1,
+    name: "pothos",
+    site: "living room",
+    last_watered: 2025-02-14,
+    watering_interval: 10
+  },
+  {
+    id: 2,
+    name: "staghorn fern",
+    site: "office",
+    last_watered: 2025-02-10,
+    watering_interval: 14
+  },
+  {
+    id: 3,
+    name: "cactus",
+    site: "front porch",
+    last_watered: 2025-02-01,
+    watering_interval: 20
+  },
+  { DEAD PLANT
+    id: 4,
+    name: "daisy",
+    site: "graveyard",
+    last_watered: null,
+    watering_interval: null
+  },
+]
+*/
 
 export default function Page() {
   const { plants, loading } = useData();
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [newPlantName, setNewPlantName] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    if (plants) {
+      console.log(sortPlants(plants));
+    }
+  }, plants);
+
+  function sortPlants(plantArray) {
+    let sortedPlants = plantArray.sort();
+
+    return plantArray;
+  }
 
   const createPlant = async () => {
     const response = await fetch("/api/plants", {
@@ -31,13 +85,17 @@ export default function Page() {
       createPlant();
     }
   }
-  // if (loading) return <div>Loading plants...</div>;
+
+  if (loading) return <div>Loading plants...</div>;
 
   return (
     <div>
+      <p>Current date and time: {currentDate.toLocaleString()}</p>
       <div>
         {plants.map((plant) => (
-          <div key={plant.id}>{plant.name}</div>
+          <div key={plant.id}>
+            {plant.name} - {plant.last_watered - plant.watering_interval}
+          </div>
         ))}
       </div>
       {/* <div>
